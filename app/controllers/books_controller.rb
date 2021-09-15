ba16a092710fcb516d0b0ce2b5df55143bfa5a67
masaharu-murakami <puraema8@gmail.com>
2021-09-15 14:09:16 +0000

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,63 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all.order(created_at: :desc) 
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path(book)
+
+  end
+
+
+  def create
+   @book = Book.new(book_params)
+  
+   if @book.save
+    redirect_to book_path(@book.id), notice: 'Book was successfully created.'  
+   else 
+    @books = Book.all
+   
+  
+  
+     render:index
+   end
+
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+   
+    
+    if @book.update(book_params)
+    redirect_to book_path(@book.id), notice: 'Book was successfully updated.'  
+    else
+      
+    render:edit
+    end
+    
+  end
+
+  private
+   def book_params
+    params.require(:book).permit(:title,:body)
+   end
+
+
+
+  
+end
+
